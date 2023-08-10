5ff3a0ad2d8cd6978a59e627b00fbe9f3f8a8e97
EdwinBayingana <bayinganaedwin@gmail.com>
2023-08-10 10:22:38 +0200

    render the task component in App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
   return (
@@ -10,6 +10,7 @@ export default function App() {
 
         <View style={styles.items}>
           {/* Tasks will be displayed down here */}
+          <Task />
         </View>
       </View>
     </View>
