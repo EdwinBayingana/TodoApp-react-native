5ef02c98341f05c05fda8c8cf39bcc0468d8d965
EdwinBayingana <bayinganaedwin@gmail.com>
2023-08-10 12:02:41 +0200

    finish the Task component design

--- components/Task.js
+++ components/Task.js
@@ -14,6 +14,39 @@ const Task = ({ text }) => {
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: '#ffffff',
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+  },
+  itemLeft: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+  },
+  square: {
+    height: 24,
+    width: 24,
+    backgroundColor: '#55BCF6',
+    opacity: 0.4,
+    borderRadius: 5,
+    marginRight: 15,
+  },
+  itemText: {
+    maxWidth: '80%',
+  },
+  circular: {
+    width: 13,
+    height: 13,
+    borderColor: '#55BCF6',
+    borderWidth: 2,
+    borderRadius: 5,
+  },
+});
 
 export default Task;
